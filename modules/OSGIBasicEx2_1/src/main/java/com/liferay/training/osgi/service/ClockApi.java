0f4c9a003cc921a068af200b61c1810559724e37
dnovoseltsev <dnovoseltsev@emdev.ru>
2022-04-21 17:06:57 +0700

    add module OSGIBasicEx2_1

---
+++ modules/OSGIBasicEx2_1/src/main/java/com/liferay/training/osgi/service/ClockApi.java
@@ -0,0 +1,5 @@
+package com.liferay.training.osgi.service;
+
+public interface ClockApi {
+    public String getTime();
+}
--- modules/portletMVC/src/main/resources/META-INF/resources/view.jsp
+++
@@ -1,5 +0,0 @@
-<%@ include file="/init.jsp" %>
-
-<p>
-	<b><liferay-ui:message key="portletmvc.caption"/></b>
-</p>
