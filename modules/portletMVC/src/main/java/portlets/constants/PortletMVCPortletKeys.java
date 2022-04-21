08da634e3cf7ffd7151c0eaf36419e3ed441269c
dnovoseltsev <dnovoseltsev@emdev.ru>
2022-04-21 11:28:00 +0700

    add project layout

---
+++ modules/portletMVC/src/main/java/portlets/constants/PortletMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package portlets.constants;
+
+/**
+ * @author A
+ */
+public class PortletMVCPortletKeys {
+
+	public static final String PORTLETMVC =
+		"portlets_PortletMVCPortlet";
+
+}
---
+++ modules/portletMVC/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="portletmvc.caption"/></b>
+</p>
