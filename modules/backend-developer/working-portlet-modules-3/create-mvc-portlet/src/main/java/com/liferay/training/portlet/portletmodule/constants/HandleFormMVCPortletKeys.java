75260f64c71f373940bbe609c383c4d0556c7c25
dnovoseltsev <dnovoseltsev@emdev.ru>
2022-04-22 16:31:38 +0700

    add module backend-developer.working-portlet-modules-3.create-mvc-portlet

---
+++ modules/backend-developer/working-portlet-modules-3/create-mvc-portlet/src/main/java/com/liferay/training/portlet/portletmodule/constants/HandleFormMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.portlet.portletmodule.constants;
+
+/**
+ * @author A
+ */
+public class HandleFormMVCPortletKeys {
+
+	public static final String HANDLEFORMMVC =
+		"com_liferay_training_portlet_portletmodule_HandleFormMVCPortlet";
+
+}
