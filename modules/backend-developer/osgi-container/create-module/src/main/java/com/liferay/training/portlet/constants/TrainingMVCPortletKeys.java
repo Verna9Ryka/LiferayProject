1675cdfafb49494a93e19853e97f5c99f26fb5f3
dnovoseltsev <dnovoseltsev@emdev.ru>
2022-04-22 13:40:36 +0700

    add module backend-developer.osgi-container.create-module

---
+++ modules/backend-developer/osgi-container/create-module/src/main/java/com/liferay/training/portlet/constants/TrainingMVCPortletKeys.java
@@ -0,0 +1,11 @@
+package com.liferay.training.portlet.constants;
+
+/**
+ * @author A
+ */
+public class TrainingMVCPortletKeys {
+
+	public static final String TRAININGMVC =
+		"com_liferay_training_portlet_TrainingMVCPortlet";
+
+}
---
+++ modules/backend-developer/osgi-container/create-module/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/META-INF/resources/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="trainingmvc.caption"/></b>
+</p>
+
+<p>Hello Liferay!</p>
